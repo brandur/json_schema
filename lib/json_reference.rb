fbc9835b06cce7e72add9b7dfbc13f0c636cd94d
Brandur <brandur@mutelight.org>
2014-05-09 14:59:25 +0200

    JSON Reference utils

--- lib/json_reference.rb
+++ lib/json_reference.rb
@@ -1,2 +1,31 @@
+require "json_pointer"
+require "uri"
+
 module JsonReference
+  class Reference
+    attr_accessor :pointer
+    attr_accessor :uri
+
+    def initialize(ref)
+      # given a simple fragment without '#', resolve as a JSON Pointer only as
+      # per spec
+      if ref.include?("#")
+        uri, @pointer = ref.split('#')
+        if uri && !uri.empty?
+          @uri = URI.parse(uri)
+        end
+      else
+        @pointer = ref
+      end
+
+      # normalize pointers by prepending "#"
+      @pointer = "#" + @pointer
+    end
+
+    # Given the document addressed by #uri, resolves the JSON Pointer part of
+    # the reference.
+    def resolve_pointer(data)
+      JsonPointer.evaluate(data, @pointer)
+    end
+  end
 end
