f629bc6ffff78220d060619201c13a4d4031724e
Brandur <brandur@mutelight.org>
2014-05-13 01:24:30 +0200

    Remove JSON version of evaluation from pointer

--- lib/json_pointer.rb
+++ lib/json_pointer.rb
@@ -4,9 +4,4 @@ module JsonPointer
   def self.evaluate(data, path)
     Evaluator.new(data).evaluate(path)
   end
-
-  def self.evaluate_json(json, path)
-    data = MultiJson.decode(json)
-    Evaluator.new(data).evaluate(path)
-  end
 end
