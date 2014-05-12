9cdcfe6cf97324c1cb0571d246c424ae1f1fa053
Brandur <brandur@mutelight.org>
2014-05-13 01:15:39 +0200

    More consistent testing conventions

--- test/json_schema/parser_test.rb
+++ test/json_schema/parser_test.rb
@@ -3,12 +3,8 @@ require "test_helper"
 require "json_schema"
 
 describe JsonSchema::Parser do
-  before do
-    @parser = JsonSchema::Parser.new
-  end
-
   it "parses the basic attributes of a schema" do
-    schema = @parser.parse!(schema_sample)
+    schema = parse
     assert_nil schema.id
     assert_equal "Example API", schema.title
     assert_equal "An example API.", schema.description
@@ -17,7 +13,7 @@ describe JsonSchema::Parser do
   end
 
   it "parses subschemas" do
-    schema = @parser.parse!(schema_sample).definitions["app"]
+    schema = parse.definitions["app"]
     assert_nil schema.reference
     assert_equal "App", schema.title
     assert_equal "An app.", schema.description
@@ -28,7 +24,7 @@ describe JsonSchema::Parser do
   end
 
   it "parses sub-subschemas" do
-    schema = @parser.parse!(schema_sample).definitions["app"].definitions["name"]
+    schema = parse.definitions["app"].definitions["name"]
     assert_nil schema.reference
     assert_equal "unique name of app", schema.description
     assert_equal ["string"], schema.type
@@ -37,7 +33,7 @@ describe JsonSchema::Parser do
   end
 
   it "parses references" do
-    schema = @parser.parse!(schema_sample).properties["app"]
+    schema = parse.properties["app"]
     refute_nil schema.reference
     assert_nil schema.reference.uri
     assert_equal "#/definitions/app", schema.reference.pointer
@@ -45,19 +41,19 @@ describe JsonSchema::Parser do
   end
 
   it "parses enum validation" do
-    schema = @parser.parse!(schema_sample).definitions["app"].definitions["visibility"]
+    schema = parse.definitions["app"].definitions["visibility"]
     assert_equal ["private", "public"], schema.enum
   end
 
   it "parses array validations" do
-    schema = @parser.parse!(schema_sample).definitions["app"].definitions["flags"]
+    schema = parse.definitions["app"].definitions["flags"]
     assert_equal 1, schema.min_items
     assert_equal 10, schema.max_items
     assert_equal true, schema.unique_items
   end
 
   it "parses integer validations" do
-    schema = @parser.parse!(schema_sample).definitions["app"].definitions["id"]
+    schema = parse.definitions["app"].definitions["id"]
     assert_equal 0, schema.min
     assert_equal true, schema.min_exclusive
     assert_equal 10000, schema.max
@@ -66,7 +62,7 @@ describe JsonSchema::Parser do
   end
 
   it "parses number validations" do
-    schema = @parser.parse!(schema_sample).definitions["app"].definitions["cost"]
+    schema = parse.definitions["app"].definitions["cost"]
     assert_equal 0.0, schema.min
     assert_equal false, schema.min_exclusive
     assert_equal 1000.0, schema.max
@@ -75,7 +71,7 @@ describe JsonSchema::Parser do
   end
 
   it "parses the basic set of object validations" do
-    schema = @parser.parse!(schema_sample).definitions["app"]
+    schema = parse.definitions["app"]
     assert_equal false, schema.additional_properties
     assert_equal 10, schema.max_properties
     assert_equal 1, schema.min_properties
@@ -83,13 +79,13 @@ describe JsonSchema::Parser do
   end
 
   it "parses the dependencies object validation" do
-    schema = @parser.parse!(schema_sample).definitions["app"]
+    schema = parse.definitions["app"]
     assert_equal ["ssl"], schema.dependencies["production"]
     assert_equal 20.0, schema.dependencies["ssl"].properties["cost"].min
   end
 
   it "parses the patternProperties object validation" do
-    schema = @parser.parse!(schema_sample).definitions["app"].definitions["config_vars"]
+    schema = parse.definitions["app"].definitions["config_vars"]
     property = schema.pattern_properties.first
     assert_equal /^\w+$/, property[0]
     assert_equal ["null", "string"], property[1].type
@@ -97,67 +93,67 @@ describe JsonSchema::Parser do
 
   # couldn't think of any non-contrived examples to work with here
   it "parses the basic set of schema validations" do
-    schema = @parser.parse!(schema_sample).definitions["app"].definitions["contrived"]
+    schema = parse.definitions["app"].definitions["contrived"]
     assert_equal 2, schema.all_of.count
     assert_equal 2, schema.one_of.count
     assert schema.not
   end
 
   it "parses the anyOf schema validation" do
-    schema = @parser.parse!(schema_sample).definitions["app"].definitions["identity"]
+    schema = parse.definitions["app"].definitions["identity"]
     assert_equal 2, schema.any_of.count
     assert_equal "/schemata/app#/definitions/id", schema.any_of[0].reference.to_s
     assert_equal "/schemata/app#/definitions/name", schema.any_of[1].reference.to_s
   end
 
   it "parses basic set of string validations" do
-    schema = @parser.parse!(schema_sample).definitions["app"].definitions["name"]
+    schema = parse.definitions["app"].definitions["name"]
     assert_equal 30, schema.max_length
     assert_equal 3, schema.min_length
     assert_equal /^[a-z][a-z0-9-]{3,30}$/, schema.pattern
   end
 
   it "errors on non-string ids" do
-    local_schema_sample = schema_sample.dup
-    local_schema_sample["id"] = 4
-    e = assert_raises(RuntimeError) { @parser.parse!(local_schema_sample) }
+    schema_sample["id"] = 4
+    e = assert_raises(RuntimeError) { parse }
     assert_equal %{At "/": Expected "id" to be of type "string"; value was: 4.},
       e.message
   end
 
   it "errors on non-string titles" do
-    local_schema_sample = schema_sample.dup
-    local_schema_sample["title"] = 4
-    e = assert_raises(RuntimeError) { @parser.parse!(local_schema_sample) }
+    schema_sample["title"] = 4
+    e = assert_raises(RuntimeError) { parse }
     assert_equal %{At "/": Expected "title" to be of type "string"; value was: 4.},
       e.message
   end
 
   it "errors on non-string descriptions" do
-    local_schema_sample = schema_sample.dup
-    local_schema_sample["description"] = 4
-    e = assert_raises(RuntimeError) { @parser.parse!(local_schema_sample) }
+    schema_sample["description"] = 4
+    e = assert_raises(RuntimeError) { parse }
     assert_equal %{At "/": Expected "description" to be of type "string"; value was: 4.},
       e.message
   end
 
   it "errors on non-array and non-string types" do
-    local_schema_sample = schema_sample.dup
-    local_schema_sample["type"] = 4
-    e = assert_raises(RuntimeError) { @parser.parse!(local_schema_sample) }
+    schema_sample["type"] = 4
+    e = assert_raises(RuntimeError) { parse }
     assert_equal %{At "/": Expected "type" to be of type "array/string"; value was: 4.},
       e.message
   end
 
   it "errors on unknown types" do
-    local_schema_sample = schema_sample.dup
-    local_schema_sample["type"] = ["float", "double"]
-    e = assert_raises(RuntimeError) { @parser.parse!(local_schema_sample) }
+    schema_sample["type"] = ["float", "double"]
+    e = assert_raises(RuntimeError) { parse }
     assert_equal %{At "/": Unknown types: double, float.},
       e.message
   end
 
+  def parse
+    @parser = JsonSchema::Parser.new
+    @parser.parse!(schema_sample)
+  end
+
   def schema_sample
-    DataScaffold.schema_sample
+    @schema_sample ||= DataScaffold.schema_sample
   end
 end
--- test/json_schema/reference_expander_test.rb
+++ test/json_schema/reference_expander_test.rb
@@ -4,7 +4,7 @@ require "json_schema"
 
 describe JsonSchema::ReferenceExpander do
   it "expands references" do
-    expand(schema_sample)
+    expand
     # this was always a fully-defined property
     referenced = @schema.definitions["app"]
     # this used to be a $ref
@@ -18,40 +18,40 @@ describe JsonSchema::ReferenceExpander do
   end
 
   it "will expand anyOf" do
-    expand(schema_sample)
+    expand
     schema = @schema.properties["app"].definitions["contrived_plus"]
     assert_equal 3, schema.any_of[0].min_length
     assert_equal 5, schema.any_of[1].min_length
   end
 
   it "will expand allOf" do
-    expand(schema_sample)
+    expand
     schema = @schema.properties["app"].definitions["contrived_plus"]
     assert_equal 30, schema.all_of[0].max_length
     assert_equal 3, schema.all_of[1].min_length
   end
 
   it "will expand dependencies" do
-    expand(schema_sample)
+    expand
     schema = @schema.properties["app"].dependencies["ssl"].properties["name"]
     assert_equal ["string"], schema.type
   end
 
   it "will expand oneOf" do
-    expand(schema_sample)
+    expand
     schema = @schema.properties["app"].definitions["contrived_plus"]
     assert_equal /^(foo|aaa)$/, schema.one_of[0].pattern
     assert_equal /^(foo|zzz)$/, schema.one_of[1].pattern
   end
 
   it "will expand not" do
-    expand(schema_sample)
+    expand
     schema = @schema.properties["app"].definitions["contrived_plus"]
     assert_equal /^$/, schema.not.pattern
   end
 
   it "will expand patternProperties" do
-    expand(schema_sample)
+    expand
     # value ([1]) of the #first tuple in hash
     schema = @schema.properties["app"].definitions["roles"].
       pattern_properties.first[1]
@@ -59,8 +59,7 @@ describe JsonSchema::ReferenceExpander do
   end
 
   it "will perform multiple passes to resolve all references" do
-    local_schema_sample = schema_sample.dup
-    local_schema_sample["properties"] = {
+    schema_sample["properties"] = {
       "app" => {
         "$ref" => "#/properties/my-app"
       },
@@ -68,49 +67,40 @@ describe JsonSchema::ReferenceExpander do
         "$ref" => "#/definitions/app"
       }
     }
-    expand(local_schema_sample)
+    expand
   end
 
   it "errors on a JSON Pointer that can't be resolved" do
-    local_schema_sample = schema_sample.dup
-    local_schema_sample["properties"]["app"] = {
+    schema_sample["properties"]["app"] = {
       "$ref" => "#/definitions/nope"
     }
-    e = assert_raises(RuntimeError) do
-      expand(local_schema_sample)
-    end
+    e = assert_raises(RuntimeError) { expand }
     assert_equal %{At "/": Couldn't resolve pointer "#/definitions/nope".},
       e.message
   end
 
   it "errors on a schema that can't be resolved" do
-    local_schema_sample = schema_sample.dup
-    local_schema_sample["properties"]["app"] = {
+    schema_sample["properties"]["app"] = {
       "$ref" => "/schemata/user#/definitions/name"
     }
-    e = assert_raises(RuntimeError) do
-      expand(local_schema_sample)
-    end
+    e = assert_raises(RuntimeError) { expand }
     assert_equal %{At "/": Couldn't resolve references (possible circular dependency): /schemata/user#/definitions/name.},
       e.message
   end
 
   it "errors on a circular reference" do
-    local_schema_sample = schema_sample.dup
-    local_schema_sample["definitions"]["app"] = {
+    schema_sample["definitions"]["app"] = {
       "$ref" => "#/properties/app"
     }
-    e = assert_raises(RuntimeError) do
-      expand(local_schema_sample)
-    end
+    e = assert_raises(RuntimeError) { expand }
     assert_equal %{At "/": Couldn't resolve references (possible circular dependency): #/definitions/app.}, e.message
   end
 
   def schema_sample
-    DataScaffold.schema_sample
+    @schema_sample ||= DataScaffold.schema_sample
   end
 
-  def expand(schema_sample)
+  def expand
     @schema = JsonSchema::Parser.new.parse!(schema_sample)
     @expander = JsonSchema::ReferenceExpander.new(@schema)
     @expander.expand!
