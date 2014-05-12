06bbccdd589af5c1ec7028619c210c9534dad15b
Brandur <brandur@mutelight.org>
2014-05-12 23:06:35 +0200

    Significantly cleaner tests

--- test/json_schema/validator_test.rb
+++ test/json_schema/validator_test.rb
@@ -4,48 +4,44 @@ require "json_schema"
 
 describe JsonSchema::Validator do
   it "can find data valid" do
-    assert validate(data_sample)
+    assert validate
   end
 
   it "validates type" do
-    refute validate(4)
+    @data_sample = 4
+    refute validate
     assert_includes error_messages,
       %{Expected data to be of type "object"; value was: 4.}
   end
 
   it "validates maxItems" do
-    local_data = data_sample.dup
-    local_data["flags"] = (0...11).to_a
-    refute validate(local_data)
+    data_sample["flags"] = (0...11).to_a
+    refute validate
     assert_includes error_messages,
       %{Expected array to have no more than 10 item(s), had 11 item(s).}
   end
 
   it "validates minItems" do
-    local_data = data_sample.dup
-    local_data["flags"] = []
-    refute validate(local_data)
+    data_sample["flags"] = []
+    refute validate
     assert_includes error_messages,
       %{Expected array to have at least 1 item(s), had 0 item(s).}
   end
 
   it "validates uniqueItems" do
-    local_data = data_sample.dup
-    local_data["flags"] = [1, 1]
-    refute validate(local_data)
+    data_sample["flags"] = [1, 1]
+    refute validate
     assert_includes error_messages,
       %{Expected array items to be unique, but duplicate items were found.}
   end
 
   it "validates maximum for an integer with exclusiveMaximum false" do
-    local_schema = schema_sample.dup
-    id = pointer(local_schema, "#/definitions/app/definitions/id").merge!(
+    id = pointer(schema_sample, "#/definitions/app/definitions/id").merge!(
       "exclusiveMaximum" => false,
       "maximum"          => 10
     )
-    local_data = data_sample.dup
-    local_data["id"] = 11
-    refute validate(local_data, local_schema)
+    data_sample["id"] = 11
+    refute validate
     assert_includes error_messages,
       %{Expected data to be smaller than maximum 10 (exclusive: false), value was: 11.}
   end
@@ -72,7 +68,7 @@ describe JsonSchema::Validator do
   end
 
   def data_sample
-    DataScaffold.data_sample
+    @data_sample ||= DataScaffold.data_sample
   end
 
   def error_messages
@@ -84,11 +80,10 @@ describe JsonSchema::Validator do
   end
 
   def schema_sample
-    DataScaffold.schema_sample
+    @schema_sample ||= DataScaffold.schema_sample
   end
 
-  def validate(data_sample, schema_sample = nil)
-    schema_sample ||= DataScaffold.schema_sample
+  def validate
     @schema = JsonSchema.parse!(schema_sample).definitions["app"]
     @validator = JsonSchema::Validator.new(@schema)
     @validator.validate(data_sample)
