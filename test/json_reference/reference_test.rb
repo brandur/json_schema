6693645956193b7aa9429190b54ee1a220995bea
Brandur <brandur@mutelight.org>
2014-05-20 16:04:38 +0200

    JSON Reference tests

---
+++ test/json_reference/reference_test.rb
@@ -0,0 +1,45 @@
+require "test_helper"
+
+require "json_reference"
+
+describe JsonReference::Reference do
+  it "expands a reference without a URI" do
+    ref = reference("#/definitions")
+    assert_equal nil, ref.uri
+    assert_equal "#/definitions", ref.pointer
+  end
+
+  it "expands a reference with a URI" do
+    ref = reference("http://example.com#/definitions")
+    assert_equal URI.parse("http://example.com"), ref.uri
+    assert_equal "#/definitions", ref.pointer
+  end
+
+  it "expands just a root sign" do
+    ref = reference("#")
+    assert_equal nil, ref.uri
+    assert_equal "#", ref.pointer
+  end
+
+  it "expands a URI with just a root sign" do
+    ref = reference("http://example.com#")
+    assert_equal URI.parse("http://example.com"), ref.uri
+    assert_equal "#", ref.pointer
+  end
+
+  it "normalizes pointers by adding a root sign prefix" do
+    ref = reference("/definitions")
+    assert_equal nil, ref.uri
+    assert_equal "#/definitions", ref.pointer
+  end
+
+  it "normalizes pointers by stripping a trailing slash" do
+    ref = reference("#/definitions/")
+    assert_equal nil, ref.uri
+    assert_equal "#/definitions", ref.pointer
+  end
+
+  def reference(str)
+    JsonReference::Reference.new(str)
+  end
+end
