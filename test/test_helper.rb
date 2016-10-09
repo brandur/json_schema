93ecc992e01a894880e876fbb16be75b60d91ed3
Brandur <brandur@mutelight.org>
2016-10-08 18:33:52 -0600

    Basic testing on executables
    Loads executables in the test suite so that we at least get a basic
    check that their syntax is correct.
    
    Exclude executables from code coverage measurements because it's nigh
    impossible to get all lines.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 if RUBY_VERSION >= '2.0.0'
   require 'simplecov'
   SimpleCov.start do
+    # We do our utmost to test our executables by modularizing them into
+    # testable pieces, but testing them to completion is nearly impossible as
+    # far as I can tell, so include them in our tests but don't calculate
+    # coverage.
+    add_filter "/bin/"
+
     add_filter "/test/"
   end
 end
