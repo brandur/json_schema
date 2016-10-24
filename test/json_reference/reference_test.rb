7cfcbe51b05c36a4fe24f874aa8f4d9eb2326ce1
Brandur <brandur@mutelight.org>
2016-10-24 13:14:46 -0700

    Fix tests

--- test/json_reference/reference_test.rb
+++ test/json_reference/reference_test.rb
@@ -6,37 +6,37 @@ describe JsonReference::Reference do
   it "expands a reference without a URI" do
     ref = reference("#/definitions")
     assert_equal nil, ref.uri
-    assert_equal "#/definitions", ref.pointer
+    assert_equal "#/definitions", ref.reference_pointer
   end
 
   it "expands a reference with a URI" do
     ref = reference("http://example.com#/definitions")
     assert_equal URI.parse("http://example.com"), ref.uri
-    assert_equal "#/definitions", ref.pointer
+    assert_equal "#/definitions", ref.reference_pointer
   end
 
   it "expands just a root sign" do
     ref = reference("#")
     assert_equal nil, ref.uri
-    assert_equal "#", ref.pointer
+    assert_equal "#", ref.reference_pointer
   end
 
   it "expands a URI with just a root sign" do
     ref = reference("http://example.com#")
     assert_equal URI.parse("http://example.com"), ref.uri
-    assert_equal "#", ref.pointer
+    assert_equal "#", ref.reference_pointer
   end
 
   it "normalizes pointers by adding a root sign prefix" do
     ref = reference("/definitions")
     assert_equal nil, ref.uri
-    assert_equal "#/definitions", ref.pointer
+    assert_equal "#/definitions", ref.reference_pointer
   end
 
   it "normalizes pointers by stripping a trailing slash" do
     ref = reference("#/definitions/")
     assert_equal nil, ref.uri
-    assert_equal "#/definitions", ref.pointer
+    assert_equal "#/definitions", ref.reference_pointer
   end
 
   def reference(str)
