brandur/json_schema

commit d110c244b7d3d5121273fa52b636095edbdbac22

precalculate valid Ruby types to speed up validation (#94)
* precalculate valid Ruby types to speed up validation

* add back nil check (no perf difference)

* clear dependently-cached attributes when assigning type

lib/json_schema/validator.rb
@@ -2,16 +2,6 @@ require "uri"
 
 module JsonSchema
   class Validator
-    TYPE_MAP = {
-      "array"   => Array,
-      "boolean" => [FalseClass, TrueClass],
-      "integer" => Integer,
-      "number"  => [Integer, Float],
-      "null"    => NilClass,
-      "object"  => Hash,
-      "string"  => String,
-    }
-
     attr_accessor :errors
 
     def initialize(schema)
@@ -512,8 +502,7 @@ module JsonSchema
 
     def validate_type(schema, data, errors, path)
       return true if !schema.type || schema.type.empty?
-      valid_types = schema.type.flat_map { |t| TYPE_MAP[t] }.compact
-      if valid_types.any? { |t| data.is_a?(t) }
+      if schema.type_parsed.include?(data.class)
         true
       else
         key = find_parent(schema)

lib/json_schema/attributes.rb
@@ -43,10 +43,18 @@ module JsonSchema
             end
           end
         end
+
+        if options[:clear_cache]
+          remove_method(:"#{attr}=")
+          define_method(:"#{attr}=") do |value|
+            instance_variable_set(options[:clear_cache], nil)
+            instance_variable_set(ref, value)
+          end
+        end
       end
 
       def attr_schema(attr, options = {})
-        attr_copyable(attr, :default => options[:default])
+        attr_copyable(attr, :default => options[:default], :clear_cache => options[:clear_cache])
         self.schema_attrs[options[:schema_name] || attr] = attr
       end
 

lib/json_schema/schema.rb
@@ -2,6 +2,16 @@ require "json"
 
 module JsonSchema
   class Schema
+    TYPE_MAP = {
+      "array"   => Array,
+      "boolean" => [FalseClass, TrueClass],
+      "integer" => Integer,
+      "number"  => [Integer, Float],
+      "null"    => NilClass,
+      "object"  => Hash,
+      "string"  => String,
+    }
+
     include Attributes
 
     def initialize
@@ -129,7 +139,7 @@ module JsonSchema
     # of strings.
     #
     # Type: Array[String]
-    attr_schema :type, :default => []
+    attr_schema :type, :default => [], :clear_cache => :@type_parsed
 
     # validation: array
     attr_schema :additional_items, :default => true, :schema_name => :additionalItems
@@ -197,6 +207,14 @@ module JsonSchema
     def expand_references!(options = {})
       ReferenceExpander.new.expand!(self, options)
       true
+    end
+
+    # An array of Ruby classes that are equivalent to the types defined in the
+    # schema.
+    #
+    # Type: Array[Class]
+    def type_parsed
+      @type_parsed ||= type.flat_map { |t| TYPE_MAP[t] }.compact
     end
 
     def inspect

test/json_schema/attribute_test.rb
@@ -14,7 +14,7 @@ describe JsonSchema::Attributes do
     obj = TestAttributes.new
     obj.schema = "foo"
     assert_equal "foo", obj.schema
-    assert_equal({:schema => :schema, :named => :schema_named},
+    assert_equal({:schema => :schema, :named => :schema_named, :cached => :cached},
       obj.class.schema_attrs)
   end
 
@@ -75,6 +75,16 @@ describe JsonSchema::Attributes do
     assert_nil obj.schema
   end
 
+  it "cleans cached values when assigning parent attribute" do
+    obj = TestAttributes.new
+
+    obj.cached = "test"
+    assert_equal "test_123", obj.cached_parsed
+
+    obj.cached = "other"
+    assert_equal "other_123", obj.cached_parsed
+  end
+
   class TestAttributes
     include JsonSchema::Attributes
 
@@ -86,6 +96,11 @@ describe JsonSchema::Attributes do
 
     attr_schema :schema
     attr_schema :schema_named, :schema_name => :named
+
+    attr_schema :cached, :clear_cache => :@cached_parsed
+    def cached_parsed
+      @cached_parsed ||= "#{cached}_123"
+    end
 
     attr_copyable :copyable_default, :default => []
     attr_copyable :copyable_default_with_string, :default => "application/json"

test/json_schema/schema_test.rb
@@ -34,4 +34,13 @@ describe JsonSchema::Schema do
       schema[:expanded]
     end
   end
+
+  it "updates type_parsed when type is changed" do
+    schema = JsonSchema::Schema.new
+    schema.type = ["integer"]
+    assert_equal [Integer], schema.type_parsed
+
+    schema.type = ["string"]
+    assert_equal [String], schema.type_parsed
+  end
 end

commit a8d65d2168053540d172e49a9b5be2a71aa1d3fd

allow subtypes again when validating types (fixes #97)
This fault was originally introduced in d110c244b7d3d5121273fa52b636095edbdbac22.

lib/json_schema/validator.rb
@@ -526,7 +526,7 @@ module JsonSchema
 
     def validate_type(schema, data, errors, path)
       return true if !schema.type || schema.type.empty?
-      if schema.type_parsed.include?(data.class)
+      if schema.type_parsed.any? { |t| data.is_a?(t) }
         true
       else
         key = find_parent(schema)

test/json_schema/validator_test.rb
@@ -38,6 +38,16 @@ describe JsonSchema::Validator do
     assert_valid
   end
 
+  it "validates sub-type successfully" do
+    pointer("#/definitions/app").merge!(
+      "type" => ["object"]
+    )
+    class SomeClass < Hash; end
+    @data_sample = SomeClass.new
+    @data_sample["name"] = "yayrails"
+    assert_valid
+  end
+
   it "validates type unsuccessfully" do
     pointer("#/definitions/app").merge!(
       "type" => ["object"]